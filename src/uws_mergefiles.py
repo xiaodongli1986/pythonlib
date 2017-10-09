5b360b8dc155bb03b021afa38c41bb28d11eb45e
xiaodongli <xiaodongli@kias.re.kr>
2017-10-09 16:37:24 +0800

    minor changes

---
+++ src/uws_mergefiles.py
@@ -0,0 +1,41 @@
+
+import commands
+import numpy as np
+import sys
+import stdA
+
+cmdargs = sys.argv
+
+outputfile = cmdargs[1]
+
+filenames = []
+for i in range(2,len(cmdargs)):
+	nowfilenames = commands.getoutput('ls '+cmdargs[i]).split()
+	filenames = filenames + nowfilenames
+
+print 'Merge ', len(filenames) ,'csv files:'
+for nowfile in filenames:
+	print '\t', nowfile
+print 'Will create new file:\n\t', outputfile
+
+outputf = open(outputfile, 'w')
+
+totlines = 0
+ifile = 0
+for nowfile in filenames:
+	print '\tOpening ', nowfile, '...'
+	nowf = open(nowfile,'r')
+	nowlines = 0
+	nowstr = nowf.readline()
+	if ifile == 0: outputf.write(nowstr)
+	while True:
+		nowstr = nowf.readline()
+		if nowstr == '': break
+		outputf.write(nowstr)
+		totlines += 1
+		nowlines += 1
+	print '\t\t', nowlines, 'lines read & write.'
+	nowf.close()
+	ifile += 1
+print '\t', totlines, 'lines read & write in total. Finish.'
+outputf.close()
